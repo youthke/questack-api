59561c7f9a0bdcc6622eff536cc922d2cfab2129
youthke <47767005+youthke@users.noreply.github.com>
2020-06-12 19:17:24 +0900

    送り忘れ

--- conf/db.go
+++ conf/db.go
@@ -1,1 +1,25 @@
 package conf
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+)
+
+var (
+	db *gorm.DB
+	err error
+)
+
+
+func Init(){
+	db, err = gorm.Open("mysql", conf.DB.User+":"+conf.DB.Password+"@tcp("+conf.DB.IP+")/questack?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db.BlockGlobalUpdate(true)
+}
+
+func GetDB() *gorm.DB{
+	return db
+}
+
--- conf/env.go
+++ conf/env.go
@@ -1,1 +1,34 @@
 package conf
+
+import (
+	"github.com/BurntSushi/toml"
+	"log"
+	"runtime"
+)
+
+var conf Config
+
+type (
+	Config struct {
+		DB DBConfig
+	}
+
+	DBConfig struct {
+		User string
+		Password string
+		IP string
+	}
+
+)
+
+func Setup(file string) error {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	_, err := toml.DecodeFile(file, &conf)
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+
+	return nil
+}
+
