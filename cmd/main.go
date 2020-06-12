6cbdfa0bf50b3f7485e14e7b81d39587ee74ee90
youthke <47767005+youthke@users.noreply.github.com>
2020-06-12 19:18:20 +0900

    add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/HazeyamaLab/questack-api/conf"
+	"github.com/HazeyamaLab/questack-api/pkg/server"
+)
+
+var (
+	state = flag.String("s", "local", "local/prd")
+)
+
+func main() {
+
+	flag.Parse()
+	if err := conf.Setup(fmt.Sprintf("conf/env/%s.toml", *state)); err != nil {
+		fmt.Sprintf("%s", err)
+		return
+	}
+
+	conf.Init()
+	server.Init()
+}
