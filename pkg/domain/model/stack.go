29ff239726748b2ac9f2b371789cf6cf98b10951
youthke <47767005+youthke@users.noreply.github.com>
2020-06-23 01:57:27 +0900

    fix: first to find in find mine

--- pkg/domain/model/stack.go
+++ pkg/domain/model/stack.go
@@ -4,7 +4,7 @@ type Stack struct{
 	ID string `gorm:"primary_key" json:"id"`
 	Owner Owner `json:"owner"`
 	OwnerRefer uint `gorm:"column:owner_id"`
-	Name string `gorm:"name"`
+	Name string `json:"name"`
 	Questions []Question `gorm:"foreignkey:StackRefer" json:"questions"`
 }
 
