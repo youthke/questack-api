156e6d9272734db4e885a8b8617625b547355982
youthke <47767005+youthke@users.noreply.github.com>
2021-11-04 16:56:20 +0900

    add description to stack

--- db/init/2_stack_init.sql
+++ db/init/2_stack_init.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS `stacks`;
 CREATE TABLE `stacks`(
      `id` VARCHAR(255) NOT NULL,
      `name` VARCHAR (255) NOT NULL ,
+     `description` TEXT,
      `owner_id`   INT(11) NOT NULL ,
      PRIMARY KEY (`id`),
      FOREIGN KEY (`owner_id`) REFERENCES `owners`(`id`)
