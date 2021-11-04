7a22ab76385ec8b8c8c16a3e53cbfef8a84b3cf8
youthke <47767005+youthke@users.noreply.github.com>
2021-11-04 17:01:21 +0900

    add description to stack

--- pkg/domain/model/stack.go
+++ pkg/domain/model/stack.go
@@ -5,6 +5,7 @@ type Stack struct{
 	Owner Owner `json:"owner"`
 	OwnerRefer uint `gorm:"column:owner_id"`
 	Name string `json:"name"`
+	Description string `json:description`
 	Questions []Question `gorm:"foreignkey:StackRefer" json:"questions"`
 }
 
